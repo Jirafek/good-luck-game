d797875e5aed3d20bb6a91dc7e2a46a637c613f8
Jirafek <nikolaevserafim15@gmail.com>
2022-08-23 16:55:25 +0300

    fix: without add color

--- src/components/Police.jsx
+++ src/components/Police.jsx
@@ -1,8 +1,8 @@
 
 const Police = () => {
     return (
-        <section className="sm:bg-white bg-black flex">
-            <div className="bg-black py-16 flex my-14 max-w-7xl items-center sm:flex-row flex-col">
+        <section className="sm:bg-white bg-[#1a1a1a] flex">
+            <div className="bg-[#1a1a1a] py-16 flex my-14 max-w-7xl items-center sm:flex-row flex-col">
                 <img className="h-[9rem]" src="/image/doodle.png" alt="" />
                 <p className="lg:text-[1.875rem] text-[1.375rem] text-white text-center">
                     На нашем сайте вы не увидите НИКАКОЙ рекламы. НИКАКИХ реферальных ссылок. Это должно убедить вас в том что это не обман, которых в интернете очень много.
